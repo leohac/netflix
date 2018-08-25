13535fec3159898359a7c0e0869ac8273625262d
Leonardo <leo.hac2@gmail.com>
2018-08-25 17:35:14 -0300

    Adicionando tema do projeto

---
+++ wp-content/themes/netflix/footer.php
@@ -0,0 +1,10 @@
+<?php wp_footer(); ?>
+<footer>
+	
+</footer>
+
+<script src="<?php bloginfo( 'template_url' ) ?>/js/jquery-3.2.1.min.js"></script>
+<script src="<?php bloginfo( 'template_url' ) ?>/js/bootstrap.min.js"></script>
+<script src="<?php bloginfo( 'template_url' ) ?>/js/main.js"></script>
+
+</body>
